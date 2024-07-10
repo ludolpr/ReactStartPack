91a499d6560fc09c2b6fcb97662c85fa401ddb4f
ludolpr <ludolpr@gmail.com>
2024-07-10 16:34:45 +0200

    starter pack ok

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
